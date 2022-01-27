695ef30af12ba9ddb2e3330800542a7220445b5c
Danny Singh <80272391+Dikshit-byte@users.noreply.github.com>
2022-01-27 20:55:18 +0530

    deleting stateProvider.jsx

--- src/StateProvider.jsx
+++
@@ -1,8 +0,0 @@
-import React,{Children, createContext,useContext,useReducer} from "react";
-
-export const StateContext = createContext();
-
-export const StateProvider = ({ reducer,initialState})=>(
-    <StateContext.Provider value={useReducer(reducer,initialState)}>{Children}</StateContext.Provider>
-);
-export const useStateValue = ()=> useContext(StateContext);
